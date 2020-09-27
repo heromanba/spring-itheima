90f6cff328b86b435755f8f60602aa936bb34940
heromanba <heromanba24@gmail.com>
2020-09-27 22:06:07 +0800

    problem of using reflection to create objects

--- spring/day01_easy_01jdbc/src/main/java/com/itheima/jdbc/JdbcDemo1.java
+++ spring/day01_easy_01jdbc/src/main/java/com/itheima/jdbc/JdbcDemo1.java
@@ -6,11 +6,33 @@ import java.sql.*;
 
 /**
  * 程序的耦合
+ *     耦合：
+ *         程序间的依赖关系
+ *         包括：
+ *             类之间的依赖
+ *             方法间的依赖
+ *
+ *     解耦：
+ *         降低程序间的依赖关系
+ *
+ *     实际开发中应该做到：
+ *         编译期不依赖，运行时才依赖。
+ *
+ *     解耦的思路：
+ *         第一步：使用反射来创建对象，而避免使用new关键字
+ *
+ *
  */
 public class JdbcDemo1 {
     public static void main(String[] args) throws Exception {
         //1.注册驱动
-        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        //DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        /*
+            此时有个问题：
+            字符串"com.mysql.jdbc.Driver"在代码中写死了，如果之后想要换
+            数据库比如换成Oracle数据库，那么这个驱动还是需要修改。
+        */
+        Class.forName("com.mysql.jdbc.Driver");
         //2.获取连接
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/eesy", "wangchu", "wangchu");
         //3.获取操作数据库的预处理对象
