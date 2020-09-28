13df42fae85136a34fb7ab5fb49fd05a362e652d
heromanba <heromanba24@gmail.com>
2020-09-28 22:03:09 +0800

    use factory pattern to create objects

--- spring/day01_eesy_02factory/src/main/java/com/itheima/factory/BeanFactory.java
+++ spring/day01_eesy_02factory/src/main/java/com/itheima/factory/BeanFactory.java
@@ -1,5 +1,8 @@
 package com.itheima.factory;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * 一个创建Bean对象的工厂
  *
@@ -19,6 +22,51 @@ package com.itheima.factory;
  *
  * Q：创建Bean对象的工厂到底是创建什么呢？
  * A：创建我们的service和dao对象的。
+ *
+ * Q：如何创建呢？
+ * A：
+ *     第一个：需要一个配置文件来配置我们的service和dao
+ *     第二个：通过读取配置文件中配置的内容，反射创建对象
+ *
+ *     注：
+ *         配置文件至少包含两部分：全限定类名和对应的唯一标识（key，value）
+ *
+ * Q：如何准备配置文件？
+ * A：配置文件可以是xml也可以是properties
+ *
  */
 public class BeanFactory {
+    //定义一个Properties对象
+    private static Properties props;
+
+    //使用静态代码块对Properties对象赋值
+    static {
+        //1.实例化对象
+        //只能降低不可能消除new关键字带来的依赖关系
+        props = new Properties();
+        //2.获取properties文件的流对象
+        //不能直接new FileInputStream，因为相对路径和绝对路径都用不了
+        InputStream in = BeanFactory.class.getClassLoader().getResourceAsStream("bean.properties");
+        try {
+            props.load(in);
+        } catch (Exception e) {
+            //一旦发生这个错误，后面就不会运行了，因为没有配置文件，任何对象都创建不出来
+            throw new ExceptionInInitializerError("初始化Properties失败！");
+        }
+    }
+
+    /**
+     * 返回类型应写Object或使用泛型，因为如果写死一个具体的返回值则此方法不沟通用。
+     * 另外还需要一个参数指明想要得到的类型，根据Bean的名称获取bean对象。
+     */
+    public static Object getBean(String beanName) {
+        Object bean = null;
+        try {
+            String beanPath = props.getProperty(beanName);
+            bean = Class.forName(beanPath).newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bean;
+    }
 }
